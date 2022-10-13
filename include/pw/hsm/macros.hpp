fed9fb67648ce9ada097aaf1463e1a6b70af6132
Patrick Wright <Patrick.Wright@Honeywell.com>
2022-10-13 11:55:28 -0500

    Added 'advanced' versions of macros for more complex state machine designs

--- include/pw/hsm/macros.hpp
+++ include/pw/hsm/macros.hpp
@@ -15,12 +15,26 @@
 * @param ... List of type-name pairs of arguments for the event in the form
 *            ([type], [name]), ...
 */
-#define PW_HSM_EVENT(sm_, name_, ...)\
+#define PW_HSM_EVENT(sm_, name_, ...) PW_HSM_EVENT_ADV(sm_, name_, _PW_HSM_VISITOR(sm_), ##__VA_ARGS__)
+
+/**
+* @brief Advanced version of @ref PW_HSM_EVENT where the visitor class name
+*        is explicitly provided
+*/	
+#define PW_HSM_EVENT_ADV(sm_, name_, visitor_, ...)\
 	class _PW_HSM_VISITOR(sm_);\
-	class name_ : public ::pw::hsm::EventBase<name_, _PW_HSM_VISITOR(sm_)> {\
+	class name_ : public ::pw::hsm::EventBase<name_, visitor_> {\
 	public:\
 		PW_MAP(_PW_HSM_EVENT,##__VA_ARGS__)\
 	}
+
+/**
+* @brief Define a visitor class named @ref name_
+* @param name_ Name of the visitor class
+* @param ... List of event class names that can be visited
+*/
+#define PW_HSM_VISITOR_ADV(name_, ...)\
+	_PW_HSM_VISITOR_IMPL(name_, ##__VA_ARGS__)
 
 /**
 * @brief Declare traits (e.g., visitor) for a state machine
@@ -28,15 +42,21 @@
 #define PW_HSM_TRAITS(name_, root_, ...)\
 	class _PW_HSM_VISITOR(name_);\
 	using _PW_HSM_ROOT(name_) = root_;\
-	_PW_HSM_VISITOR_IMPL(_PW_HSM_VISITOR(name_), ##__VA_ARGS__)
+	PW_HSM_VISITOR_ADV(_PW_HSM_VISITOR(name_), ##__VA_ARGS__)
 
 /**
 * @brief Declare a state machine class
 *
 * @param name_ The name of the state machine
 */	
-#define PW_HSM(name_)\
-	class name_ : public ::pw::hsm::StateMachine<name_, _PW_HSM_VISITOR(name_), _PW_HSM_ROOT(name_)>
+#define PW_HSM(name_) PW_HSM_ADV(name_, _PW_HSM_VISITOR(name_), _PW_HSM_ROOT(name_))
+
+/**
+* @brief Advanced version of @ref PW_HSM where the visitor class name and root
+*        state name are explicitly provided
+*/	
+#define PW_HSM_ADV(name_, visitor_, root_)\
+	class name_ : public ::pw::hsm::StateMachine<name_, visitor_, root_>
 
 /**
 * @param sm_ The name of the state machine
@@ -44,10 +64,16 @@
 * @param parent_ The name of this state's parent state (class)
 * @param ... List of child states
 */
-#define PW_HSM_STATE(sm_, name_, parent_, ...)\
-	class name_:\
-		public ::pw::hsm::State<name_, _PW_HSM_VISITOR(sm_), parent_, ##__VA_ARGS__>
+#define PW_HSM_STATE(sm_, name_, parent_, ...) PW_HSM_STATE_ADV(sm_, name_, _PW_HSM_VISITOR(sm_), parent_, ##__VA_ARGS__)
 
+/**
+* @brief Advanced version of @ref PW_HSM_STATE where the visitor class name is
+*        explicitly provided
+*/			
+#define PW_HSM_STATE_ADV(sm_, name_, visitor_, parent_, ...)\
+	class name_:\
+		public ::pw::hsm::State<name_, visitor_, parent_, ##__VA_ARGS__>
+			
 //------------------------------------------------------------------------------
 
 #define _PW_HSM_VISITOR(name_) _##name_##_VISITOR
